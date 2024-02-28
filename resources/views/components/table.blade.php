c40bfce2ce9950be4b9ede65b99c385b9f9120b3
Björn <zibo@gmx.net>
2024-02-28 12:38:17 +0100

    style Table

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,3 +1,3 @@
-<table>
+<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
    {{ $slot }}
 </table>
--- resources/views/livewire/search-user.blade.php
+++ resources/views/livewire/search-user.blade.php
@@ -1,8 +1,35 @@
  <div>
-     <input wire:model.live="firma" placeholder="Firma">
-     <input wire:model.live="vorname" placeholder="Vorname">
-     <input wire:model.live="nachname" placeholder="Nachname">
-     <input wire:model.live="ort" placeholder="Ort">
+ 
+<form>
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div>
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Firma</label>
+            <input 
+            wire:model.live="firma"
+            type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Karl Gustav GmbH"  />
+        </div>
+        <div>
+            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Vorname</label>
+            <input
+            wire:model.live="vorname"
+             type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John"  />
+        </div>
+        <div>
+            <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nachname</label>
+            <input
+            wire:model.live="nachname"
+             type="text" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Schmidt"  />
+        </div>  
+        <div>
+            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ort</label>
+            <input
+            wire:model.live="ort"
+             type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="01** *** ** ***" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"  />
+        </div>
+       
+    </div>
+    
+ </form>
      <x-table class="table">
          <thead>
              <tr>
